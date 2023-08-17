ccc0386549104a0f63b0bf5a1e80107e34bf0ad7
lg <leonardoagustingimenez@gmail.com>
2023-08-16 21:00:51 -0300

    debugging localStorage

--- js/products.js
+++ js/products.js
@@ -1,24 +1,40 @@
-const DATA_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+const contenido = document.querySelector('.pb-5.container');
 
-
-const contenido = document.querySelector('.pb-5.container')
-
-
-function showData(dataArray) {
-    for (const item of dataArray) {
-        contenido.innerHTML +=
-            `<div class="product">
-            <p>${item.name}</p>
-            <p>${item.description}</p>
-            <p>${item.cost} ${item.currency}</p>
-            <p>Sold: ${item.soldCount}</p>
-            <img src="${item.image}" alt="Imagen del producto">
-            </div>`;
+function showData(dataArray){
+    contenido.innerHTML = "";
+    for (const item of dataArray){
+        contenido.innerHTML += `
+            <div class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${item.image}" alt="Imagen del producto" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${item.name} - ${item.currency} ${item.cost}</h4>
+                            <small class="text-muted">${item.soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${item.description}</p>
+                    </div>
+                </div>
+            </div>
+        `;
     }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    const storedCatID = localStorage.getItem("catID");
 
-fetch(DATA_URL)
-  .then(response => response.json())
-  .then(data => showData(data.products))
-  .catch(error => console.error("Error fetching data:", error));
+    if (storedCatID !== null) {
+        console.log("Stored catID:", storedCatID);
+    } else {
+        console.log("catID not found in local storage");
+    }
+
+    const DATA_URL = 'https://japceibal.github.io/emercado-api/cats_products/${cat}.json';
+
+    fetch(DATA_URL)
+        .then(response => response.json())
+        .then(data => showData(data.products))
+        .catch(error => console.error("Error fetching data:", error));
+});
