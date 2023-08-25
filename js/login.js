dd84587e0718264dea1f032362abbb4dae4fb854
cecialvarez20 <cecilia.alvarez98@gmail.com>
2023-08-25 19:42:34 -0300

    Email guardado en almacenamiento local

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
 document.getElementById("login").addEventListener("submit", (e) => {
+    let email = document.getElementById("floatingInput").value;
+    localStorage.setItem("email", email);
     e.preventDefault();
     sessionStorage.setItem("signedIn", "true");
     window.location.href = "index.html";    
