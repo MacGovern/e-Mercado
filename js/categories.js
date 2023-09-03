616ebee03ddcabf7b33381e970817dd88beef1ff
MacGovern <69129781+MacGovern@users.noreply.github.com>
2023-09-03 17:09:12 -0300

    El event handler para DOMContentLoaded ya existía, así que moví el código.

--- js/categories.js
+++ js/categories.js
@@ -113,6 +113,12 @@ if (sessionStorage.getItem("signedIn") !== "true")
     window.location.href = "login.html";
 else {
     document.addEventListener("DOMContentLoaded", function (e) {
+        let email = localStorage.getItem("email");
+        let nav = document.querySelector("nav.navbar");
+        let navItems = nav.getElementsByClassName("nav-item");
+        let ultimoNav = navItems[navItems.length - 1];
+        ultimoNav.innerHTML = `<a class="nav-link" href="my-profile.html">${email}</a>`;
+        
         getJSONData(CATEGORIES_URL).then(function (resultObj) {
             if (resultObj.status === "ok") {
                 currentCategoriesArray = resultObj.data
@@ -167,11 +173,3 @@ else {
         });
     });
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    let email = localStorage.getItem("email");
-    let nav = document.querySelector("nav.navbar");
-    let navItems = nav.getElementsByClassName("nav-item");
-    let ultimoNav = navItems[navItems.length - 1];
-    ultimoNav.innerHTML = `<a class="nav-link" href="my-profile.html">${email}</a>`;
-});
