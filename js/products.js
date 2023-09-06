437ec159fb693f51cf466719ca554a155f04897a
belenscarone <sbscarone@gmail.com>
2023-09-05 22:14:15 -0300

    Almacenamiento y redireccion

--- js/products.js
+++ js/products.js
@@ -49,11 +49,17 @@ function applySorting(array) {
   }
 }
 
+
+function setProducts(id) {
+  localStorage.setItem("products", id);
+  window.location = "product-info.html"
+};
+
 function showData(dataArray) { // Inserta en "contenido" los productos que se le pasan por parámetro (dataArray).
   contenido.innerHTML = '';
   for (const item of dataArray) {
     contenido.innerHTML += `
-            <div class="list-group-item list-group-item-action cursor-active">
+            <div onclick="setProducts(${dataArray.id})"class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <img src="${item.image}" alt="Imagen del producto" class="img-thumbnail">
