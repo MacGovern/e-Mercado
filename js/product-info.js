8f50e28b8b4ef7d404608da0972c8d8c3aef726a
lg <leonardoagustingimenez@gmail.com>
2023-09-06 23:02:10 -0300

    Esto por el momento no hace nada, pues todavia no existen los elemntos HTML ni la funcion showComments(). Sin embargo, espero pueda ayudarlos un poco sobre como organizarse. Respecto a como acceder al valor de commentScore, probablemente tenga que modificarse.

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,4 @@
-if(sessionStorage.getItem("signedIn") !== "true")
+if (sessionStorage.getItem("signedIn") !== "true")
     window.location.href = "login.html";
 else
     document.addEventListener("DOMContentLoaded", () => {
@@ -7,4 +7,23 @@ else
         let navItems = nav.getElementsByClassName("nav-item");
         let ultimoNav = navItems[navItems.length - 1];
         ultimoNav.innerHTML = `<a class="nav-link" href="my-profile.html">${email}</a>`;
+
+        document.getElementById('commentForm').addEventListener('submit', (e) => {
+            e.preventDefault();
+            const d = new Date();
+            const longDateTime = d.toISOString();
+            const newComment = {
+                product: parseInt(localStorage.getItem('productID')),
+                score: document.getElementById('commentScore').valueAsNumber,
+                description: document.getElementById('commentDescription').value,
+                user: localStorage.getItem('email'),
+                dateTime: `${longDateTime.slice(0, 10)} ${longDateTime.slice(11, 19)}`
+            }
+            let additionalComments = JSON.parse(localStorage.getItem(localStorage.getItem('productID')));
+            if (additionalComments === null)
+                additionalComments = [];
+            additionalComments.push(newComment);
+            showComments(additionalComments);
+            localStorage.setItem(localStorage.getItem('productID'), JSON.stringify(additionalComments));
+        })
     });
