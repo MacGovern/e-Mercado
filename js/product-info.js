8e89331bfc78a5eef72583f2a87cda0dc099db74
Santiamaro <santiagofederico090@gmail.com>
2023-09-11 10:31:35 -0300

    Funcionalidad de estrellas al momento de darle click.
    También se utilizo el evento mouseOver y mouseOut para darle estítica a nuestra web.

--- js/product-info.js
+++ js/product-info.js
@@ -8,6 +8,10 @@ else
         let ultimoNav = navItems[navItems.length - 1];
         ultimoNav.innerHTML = `<a class="nav-link" href="my-profile.html">${email}</a>`;
 
+
+        const starContainer = document.getElementById('starBtns');
+        const stars = Array.from(starContainer.getElementsByTagName('i'));
+        let selectedIndex;
         const commentForm = document.getElementById('commentForm');
 
         function showProductInfo(productoSeleccionado) { // Función para mostrar la información del producto seleccionado.
@@ -49,8 +53,58 @@ else
             .catch(error => {
                 console.error('Error: ', error);
             });
-            
-            commentForm.addEventListener('submit', (e) => {
+        // Empezamos con un puntaje seleccionado inicial de 0.
+        let selectedScore = 0;
+
+        stars.forEach((star, index) => {
+            star.addEventListener('click', () => {
+                if (selectedScore === index + 1) { // Si el puntaje seleccionado es igual al índice de la estrella + 1.
+                    selectedScore = 0;   // Reinicia el puntaje seleccionado a 0.
+                } else {
+                    selectedScore = index + 1; // Establece el puntaje seleccionado como el índice de la estrella + 1.
+
+                }
+
+                stars.forEach((star, i) => {
+                    if (i < selectedScore) { // Si el índice de la estrella actual es menor que el puntaje seleccionado, Cambia la clase de la estrella para mostrarla como llena.
+                        star.classList.remove('far');
+                        star.classList.add('fas');
+                        star.style.color = 'orange';// Establece el color de la estrella en naranja.
+
+                    } else { // Cambia la clase de la estrella para mostrarla como vacía
+                        star.classList.remove('fas');
+                        star.classList.add('far');
+                        star.style.color = ''; // Restablece el color de la estrella a su valor predeterminado.
+                    }
+                });
+                // actualizamos el valor del elemento HTML 'selectedScore' con el puntaje seleccionado.
+                const selectedScoreInput = document.getElementById('selectedScore');
+                if (selectedScoreInput) {
+                    selectedScoreInput.value = selectedScore;
+                }
+            });
+
+        });
+        starContainer.addEventListener('mouseover', (e) => {
+            selectedIndex = stars.indexOf(e.target);
+            for (let i = 0; i <= selectedIndex; i += 2) {
+                stars[i].style.display = 'none'; // Oculta estrella unchecked
+                stars[i + 1].style.display = 'inline-block'; // Muestra estrella checked
+            }
+        });
+
+        starContainer.addEventListener('mouseout', () => {
+            for (let i = 0; i <= selectedIndex; i += 2) {
+                stars[i].style.display = 'inline-block'; // Muestra estrella unchecked
+                stars[i + 1].style.display = 'none'; // Oculta estrella checked
+            }
+        });
+
+
+
+
+
+        commentForm.addEventListener('submit', (e) => {
             e.preventDefault();
             const d = new Date();
             const longDateTime = d.toISOString();
@@ -67,6 +121,5 @@ else
             commentForm.reset();
             // La siguiente linea esta comentada porque, hasta que showComments() no exista, las demas lineas posteriores a la invocacion de la funcion no se ejecutaran.
             // showComments(additionalComments);
-            localStorage.setItem(localStorage.getItem('productID'), JSON.stringify(additionalComments));            
-        })
+        });
     });
