c6853022cd3caa2bdec8243359dc13e7ac89ff8b
magdalena65 <141029202+magdalena65@users.noreply.github.com>
2023-09-25 14:04:48 -0300

    Agregué el menu desplegable (active)

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,5 +6,14 @@ else
         let nav = document.querySelector("nav.navbar");
         let navItems = nav.getElementsByClassName("nav-item");
         let ultimoNav = navItems[navItems.length - 1];
-        ultimoNav.innerHTML = `<a class="nav-link active" href="my-profile.html">${email}</a>`;
+        ultimoNav.innerHTML = `
+        <div class="dropdown">
+            <a class="nav-link active dropdown-toggle" href="#"" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">${email}</a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">X</a></li>
+                <li><a class="dropdown-item" href="#">X</a></li>
+                <li><a class="dropdown-item" href="#">X</a></li>
+            </ul>
+        </div>
+        `;
     });
