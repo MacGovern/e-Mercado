53884a39c1236061a10228b140d3c692dc22a049
lg <leonardoagustingimenez@gmail.com>
2023-10-03 14:27:11 -0300

    Correcciones y ahora responsive

--- js/product-info.js
+++ js/product-info.js
@@ -38,16 +38,12 @@ else
 
         function showProductInfo(productoSeleccionado) { // Función para mostrar la información del producto seleccionado.
             document.getElementById("productInfo").innerHTML += `
-                <div class="row">
-                    <div class="col">
-                        <h2 class="mt-2">${productoSeleccionado.name}
-                        </h2>
+                <div class="row mb-5">
+                    <div class="col-8 col-sm-7 col-md-8 col-lg-9 col-xl-10">
+                        <h2>${productoSeleccionado.name}</h2>
                     </div>
-                    <div class="col text-end">
-                        <button type="button" id="boton-comprar" class="btn btn-success px-3">
-                            <h6 class="mt-1">Comprar</h6>
-                        </button>
-                        </div>
+                    <div class="text-center col-4 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+                        <button type="button" id="boton-comprar" class="btn btn-success">Comprar</button>
                     </div>
                 </div>
                 <hr>
